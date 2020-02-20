ece7e0e5c9133ade729ddfd5d1b2753ba1f62c4f
Daniel da Rocha <danrocha@gmail.com>
2020-02-20 15:28:20 +0700

    fix styles

--- src/components/MailchimpForm.vue
+++ src/components/MailchimpForm.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+  <div class="mx-auto text-center md:text-left md:flex">
     <!-- Begin Mailchimp Signup Form -->
-    <p  class="mb-4">
+    <p class="mb-4 font-sans md:text-xl md:w-1/2 md:pr-4">
       <slot
-        >Assine <strong>Heloiche Lê</strong> e receba dicas de leituras a cada duas semanas, direto no seu email:</slot
+        >Assine <strong>📚Heloiche Lê</strong> e receba dicas de leituras a cada duas semanas, direto no seu email&nbsp;<span class="md:hidden">&darr;</span><span class="hidden md:inline-block">&rarr;</span></slot
       >
     </p>
     <form
@@ -12,6 +12,7 @@
       id="mc-embedded-subscribe-form"
       name="mc-embedded-subscribe-form"
       target="_blank"
+      class="mx-auto rounded sm:w-2/3 md:w-1/2 sm:p-4 sm:bg-gray-100"
       novalidate
     >
       <p class="text-left">
@@ -22,13 +23,13 @@
         <label for="mce-EMAIL" class="font-sans text-sm">Email </label>
         <input type="email" value="" name="EMAIL" class="w-full p-2 font-sans text-base border rounded shadow-inner" id="mce-EMAIL" placeholder="Email"/>
       </p>
-      <p class="flex items-center justify-between">
+      <p class="flex flex-wrap items-center justify-between">
         <input
           type="submit"
           value="Assinar"
           name="subscribe"
           id="mc-embedded-subscribe"
-          class="px-4 py-1 border rounded-full cursor-pointer hover:border-blue-700 hover:text-blue-700"
+          class="px-4 py-1 text-teal-400 bg-white border border-teal-400 rounded-full cursor-pointer hover:text-white hover:bg-teal-400"
         />
         <a
           href="https://us20.campaign-archive.com/home/?u=098dc617c09a7514b2f1ca78d&id=77c52737df"
