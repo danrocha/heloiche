43ae2af861263c8485a6e318ce7ff40485f21874
Daniel da Rocha <danrocha@gmail.com>
2020-02-20 15:39:40 +0700

    fix config

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,20 +1,19 @@
 module.exports = {
-  siteName: "Heloiche",
-  siteDescription:
-    "Avó, ateira, e leitora voraz.",
-  siteUrl: "https://heloiche.com",
+  siteName: 'Heloiche',
+  siteDescription: 'Avó, ateira, e leitora voraz.',
+  siteUrl: 'https://heloiche.com',
   titleTemplate: `%s | Heloiche`,
-  icon: "src/favicon.png",
+  icon: 'src/favicon.png',
 
   transformers: {
     remark: {
-      externalLinksTarget: "_blank",
-      externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
       plugins: [
         [
-          "gridsome-plugin-remark-shiki",
+          'gridsome-plugin-remark-shiki',
           {
-            theme: "min-light"
+            theme: 'min-light'
           }
         ]
       ]
@@ -23,101 +22,101 @@ module.exports = {
 
   plugins: [
     {
-      use: "@gridsome/vue-remark",
+      use: '@gridsome/vue-remark',
       options: {
-        baseDir: "./content/posts",
-        typeName: "Post",
-        template: "./src/templates/Post.vue",
-        pathPrefix: "/posts",
+        baseDir: './content/posts',
+        typeName: 'Post',
+        template: './src/templates/Post.vue',
+        pathPrefix: '/posts',
         refs: {
           tags: {
-            typeName: "Tag",
+            typeName: 'Tag',
             create: true
           },
           author: {
-            typeName: "Author",
+            typeName: 'Author',
             create: true
           }
         },
         plugins: [
           [
-            "gridsome-plugin-remark-shiki",
+            'gridsome-plugin-remark-shiki',
             {
-              theme: "min-light"
+              theme: 'min-light'
             }
           ]
         ]
       }
     },
     {
-      use: "@gridsome/plugin-google-analytics",
+      use: '@gridsome/plugin-google-analytics',
       options: {
-        id: "UA-135446199-1"
+        id: 'UA-158630225-1'
       }
     },
     {
-      use: "@gridsome/plugin-sitemap",
+      use: '@gridsome/plugin-sitemap',
       options: {
         cacheTime: 600000 // default
       }
     },
     {
-      use: "gridsome-plugin-rss",
+      use: 'gridsome-plugin-rss',
       options: {
-        contentTypeName: "Post",
+        contentTypeName: 'Post',
         feedOptions: {
-          title: "Bleda, a Gridsome blog starter",
-          feed_url: "https://gridsome-starter-bleda.netlify.com/feed.xml",
-          site_url: "https://gridsome-starter-bleda.netlify.com"
+          title: 'Heloiche - Avó, ateira, e leitora voraz',
+          feed_url: 'https://heloiche.com/feed.xml',
+          site_url: 'https://heloiche.com/'
         },
-        feedItemOptions: node => ({
+        feedItemOptions: (node) => ({
           title: node.title,
           description: node.description,
-          url: "https://gridsome-starter-bleda.netlify.com" + node.path,
+          url: 'https://heloiche.com' + node.path,
           author: node.author,
           date: node.date
         }),
         output: {
-          dir: "./static",
-          name: "feed.xml"
+          dir: './static',
+          name: 'feed.xml'
         }
       }
     }
   ],
 
   templates: {
-    Tag: "/tag/:id",
-    Author: "/author/:id"
+    Tag: '/tag/:id',
+    Author: '/author/:id'
   },
 
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     config.module
-      .rule("css")
-      .oneOf("normal")
-      .use("postcss-loader")
-      .tap(options => {
+      .rule('css')
+      .oneOf('normal')
+      .use('postcss-loader')
+      .tap((options) => {
         options.plugins.unshift(
           ...[
-            require("postcss-import"),
-            require("postcss-nested"),
-            require("tailwindcss")
+            require('postcss-import'),
+            require('postcss-nested'),
+            require('tailwindcss')
           ]
-        );
+        )
 
-        if (process.env.NODE_ENV === "production") {
+        if (process.env.NODE_ENV === 'production') {
           options.plugins.push(
             ...[
-              require("@fullhuman/postcss-purgecss")({
-                content: ["src/assets/**/*.css", "src/**/*.vue", "src/**/*.js"],
-                defaultExtractor: content =>
+              require('@fullhuman/postcss-purgecss')({
+                content: ['src/assets/**/*.css', 'src/**/*.vue', 'src/**/*.js'],
+                defaultExtractor: (content) =>
                   content.match(/[\w-/:%]+(?<!:)/g) || [],
                 whitelistPatterns: [/shiki/]
               })
             ]
-          );
+          )
         }
 
-        return options;
-      });
+        return options
+      })
   }
-};
+}
