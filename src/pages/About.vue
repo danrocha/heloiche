b8991b979cb16e99d9a419ad2906eabf39f2de56
Daniel da Rocha <danrocha@gmail.com>
2020-02-22 13:58:54 +0700

    implement about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,28 +1,65 @@
 <template>
   <Layout>
     <main>
-      <header class="max-w-xl md:max-w-2xl xl:max-w-3xl mx-auto text-center px-6 pt-24">
-        <h1 class="text-4xl sm:text-5xl md:text-6xl font-sans font-bold mb-1">About</h1>
-        <p class="text-grey-dark text-lg sm:text-3xl">Lorem ipsum subtitle &hellip;</p>
+      <header
+        class="max-w-xl px-6 pt-24 mx-auto md:max-w-2xl xl:max-w-3xl"
+      >
+        <h1 class="mb-1 font-sans text-4xl font-bold sm:text-5xl md:text-6xl">
+          Sobre
+        </h1>
+        <!-- <p class="text-lg text-grey-dark sm:text-3xl">
+          Lorem ipsum subtitle &hellip;
+        </p> -->
       </header>
       <nav class="absolute top-0 left-0 z-20 mt-6 ml-6">
-        <g-link to="/" class="text-gray-900 border-gray-400 text-sm border opacity-75 hover:opacity-100 rounded-full px-4 py-2 transition-opacity">&larr; Home</g-link>
+        <g-link
+          to="/"
+          class="px-4 py-2 text-sm text-gray-900 transition-opacity border border-gray-400 rounded-full opacity-75 hover:opacity-100"
+          >&larr; Voltar</g-link
+        >
       </nav>
-      <article class="max-w-xl md:max-w-2xl xl:max-w-3xl mx-auto px-6 sm:px-12 pt-16 border-b border-gray-300 pb-10 mb-16">
-        <div class="markdown text-lg leading-normal text-gray-700">
-          <p>This is your about page, say something about you or this blog.</p>
-          <p>You can edit this page in <code class="shiki-inline">src/pages/About.vue</code></p>
-          <hr>
-          <p>Now what are the possibilities of warp drive? Cmdr Riker's nervous system has been invaded by an unknown microorganism. The organisms fuse to the nerve, intertwining at the molecular level. That's why the transporter's biofilters couldn't extract it. The vertex waves show a K-complex corresponding to an REM state. The engineering section's critical. Destruction is imminent. Their robes contain ultritium, highly explosive, virtually undetectable by your transporter.</p>
-          <h2>Communication is not possible</h2>
-          <p>The shuttle has no power. Using the gravitational pull of a star to slingshot back in time? We are going to Starbase Montgomery for Engineering consultations prompted by minor read-out anomalies. Probes have recorded unusual levels of geological activity in all five planetary systems. Assemble a team. Look at records of the Drema quadrant. Would these scans detect artificial transmissions as well as natural signals?</p>
-          <p>Energy discharge in six seconds. Warp reactor core primary coolant failure. Fluctuate phaser resonance frequencies. Resistance is futile. Recommend we adjust shield harmonics to the upper EM band when proceeding. These appear to be some kind of power-wave-guide conduits which allow them to work collectively as they perform ship functions. Increase deflector modulation to upper frequency band.</p>
-          <h2>Deflector power at maximum</h2>
-          <p>Unidentified vessel travelling at sub warp speed, bearing 235.7. Fluctuations in energy readings from it, Captain. All transporters off. A strange set-up, but I'd say the graviton generator is depolarized. The dark colourings of the scrapes are the leavings of natural rubber, a type of non-conductive sole used by researchers experimenting with electricity. The molecules must have been partly de-phased by the anyon beam.</p>
-          <p>Sensors indicate human life forms 30 meters below the planet's surface. Stellar flares are increasing in magnitude and frequency. Set course for Rhomboid Dronegar 006, warp seven. There's no evidence of an advanced communication network. Total guidance system failure, with less than 24 hours' reserve power. Shield effectiveness has been reduced 12 percent. We have covered the area in a spherical pattern which a ship without warp drive could cross in the given time.</p>
-          <p>It indicates a synchronic distortion in the areas emanating triolic waves. The cerebellum, the cerebral cortex, the brain stem,  the entire nervous system has been depleted of electrochemical energy. Any device like that would produce high levels of triolic waves. These walls have undergone some kind of selective molecular polarization. I haven't determined if our phaser energy can generate a stable field. We could alter the photons with phase discriminators.</p>
-          <p>I have reset the sensors to scan for frequencies outside the usual range. By emitting harmonic vibrations to shatter the lattices. We will monitor and adjust the frequency of the resonators. He has this ability of instantly interpreting and extrapolating any verbal communication he hears. It may be due to the envelope over the structure, causing hydrogen-carbon helix patterns throughout. I'm comparing the molecular integrity of that bubble against our phasers.</p>
-          <p>Resistance is futile.</p>
+      <article
+        class="max-w-xl px-6 pt-16 pb-10 mx-auto mb-16 border-b border-gray-300 md:max-w-2xl xl:max-w-3xl sm:px-12"
+      >
+        <g-image src="../../static/images/heloiche.jpg" class="w-48 h-48 mb-8 rounded-full shadow-lg" />
+        <div class="text-lg leading-normal text-gray-700 markdown">
+          <p>
+            Estou aposentada, depois de mais de 30 anos como professora de
+            Computação da <a href="http://unicamp.br">Unicamp</a>.
+          </p>
+          <p>
+            Aposentar sempre foi aquela promessa mágica de <strong>ter o dia todo para
+            ler</strong>. Mas é claro, outras vontades e amores apareceram. Eu que sempre
+            me julguei um zero em habilidades manuais de repente me vejo
+            <strong>tricotando</strong>, <strong>“crochetando”</strong>, e até <strong>bordando</strong>. E olha que sem fazer
+            feio!
+          </p>
+          <p>
+            Também tenho os <strong>netos</strong> para encherem meu tempo de amor e doçura. Com
+            eles nasceu meu encanto pela literatura infantil e agora a juvenil.
+          </p>
+
+          <p>
+            Como eu estou "sempre inventando novidades" (segundo meu neto do
+            meio, Felipe), criei esse espaço onde me aventuro a relatar minhas
+            impressões de leituras.
+          </p>
+          <p class="font-bold">
+            É como uma conversa, bastante informal, que pretendo manter esse
+            blog e interagir com vocês que se aventurarem a me ler.
+          </p>
+          <p>
+            Sendo uma vovó "bem conectada" (segundo a Isa, a neta adolescente),
+            vocês também podem me achar no Instagram (<a
+              href="https://instagram.com/heloisavrocha"
+              >@helosavrocha</a
+            >
+            e
+            <a href="https://instagram.com/heloichefazarte">@heloichefazarte</a
+            >), no
+            <a href="https://www.facebook.com/heloisavrocha">Facebook</a>, e no
+            <a href="https://twitter.com/heloisavrocha">Twitter</a>.
+          </p>
         </div>
       </article>
       <site-footer class="pb-8 sm:pb-10" />
@@ -38,7 +75,7 @@ export default {
   components: {
     SiteFooter
   },
-  metaInfo () {
+  metaInfo() {
     return {
       title: 'About',
       meta: [
@@ -48,31 +85,31 @@ export default {
           content: this.ogDescription
         },
 
-        { property: "og:type", content: 'article' },
-        { property: "og:title", content:'About' },
-        { property: "og:description", content: this.ogDescription },
-        { property: "og:url", content: `${this.config.siteUrl}/about/` },
-        { property: "og:image", content: this.ogImageUrl },
+        { property: 'og:type', content: 'article' },
+        { property: 'og:title', content: 'About' },
+        { property: 'og:description', content: this.ogDescription },
+        { property: 'og:url', content: `${this.config.siteUrl}/about/` },
+        { property: 'og:image', content: this.ogImageUrl },
 
-        { name: "twitter:card", content: "summary_large_image" },
-        { name: "twitter:title", content: 'About' },
-        { name: "twitter:description", content: this.ogDescription },
-        { name: "twitter:site", content: "@cossssmin" },
-        { name: "twitter:creator", content: "@cossssmin" },
-        { name: "twitter:image", content: this.ogImageUrl },
-      ],
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:title', content: 'About' },
+        { name: 'twitter:description', content: this.ogDescription },
+        { name: 'twitter:site', content: '@cossssmin' },
+        { name: 'twitter:creator', content: '@cossssmin' },
+        { name: 'twitter:image', content: this.ogImageUrl }
+      ]
     }
   },
   computed: {
-    config () {
+    config() {
       return config
     },
-    ogDescription () {
+    ogDescription() {
       return 'Introduction to the Bleda blog starter for Gridsome.'
     },
-    ogImageUrl () {
+    ogImageUrl() {
       return `${this.config.siteUrl}/images/bleda-card.png`
     }
-  },
+  }
 }
 </script>
